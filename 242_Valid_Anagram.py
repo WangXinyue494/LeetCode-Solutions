0155ad89df3c8f88e14b8e8b338bbec7cdde397b
WangXinyue494 <wangxinyue494@gmail.com>
2026-02-05 10:47:51 +0800

    Create 242_Valid_Anagram.py

---
+++ 242_Valid_Anagram.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        
+        count={}
+        #key是字母   value是次数
+
+        #进货
+        for char in s:
+            if char not in count:
+                count[char]=0
+                count[char]=count[char]+1
+            else:
+                count[char]=count[char]+1
+        
+
+        #出货
+        for char in t:
+            if char not in count:
+                return False
+            else:
+                count[char]=count[char]-1
+        
+        #检查是否为0
+        for val in count.values():
+            if val!=0:
+                return False
+        
+        return True
