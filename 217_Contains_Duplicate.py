42ad68037eeb04399ab1f5adc0904df196b0431b
WangXinyue494 <wangxinyue494@gmail.com>
2026-02-06 22:14:55 +0800

    Update 217_Contains_Duplicate.py

--- 217_Contains_Duplicate.py
+++ 217_Contains_Duplicate.py
@@ -1,9 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        seen=set() #seen表示已经见过的
+        seen=set() #seen
         for num in nums:
             if num in seen:
                 return True
             else:
-                seen.add(num)   #遇到没见过的就把它加进去
+                seen.add(num)   # () for the function   [] for indexing 
         return False
